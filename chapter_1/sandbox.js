1fcc585a2f01f536aefb42ba2402a25f2e529046
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-03-15 19:12:08 +0100

    10. Common Strings Methods

--- chapter_1/sandbox.js
+++ chapter_1/sandbox.js
@@ -17,7 +17,7 @@ console.log(score)
 
 //////////////////////////
 // Lesson 9 Strings
-
+/*
 // strings
 console.log('hello, world')
 
@@ -44,3 +44,21 @@ console.log(result, fullName)
 
 let index = email.indexOf('@')
 console.log(index)
+*/
+
+//////////////////////////
+// Lesson 10 Common Strings Methods
+
+let email = 'mario@thenetninja.co.uk'
+
+// let result = email.lastIndexOf('n')
+
+// let result = email.slice(2, 5)
+
+// let result = email.substr(4, 10)
+
+// let result = email.replace('m', 'w')
+
+let result = email.replace('n', 'w')
+
+console.log(result)
