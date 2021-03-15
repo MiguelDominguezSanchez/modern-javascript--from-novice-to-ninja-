457251d5d802512c658f31623a3e188bdfaf4233
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-03-15 19:05:06 +0100

    9. Strings

--- chapter_1/sandbox.js
+++ chapter_1/sandbox.js
@@ -1,3 +1,4 @@
+/*
 let age = 25
 let year = 2019
 
@@ -9,6 +10,37 @@ console.log(age)
 
 const points = 100
 console.log(points) */
-
+/*
 var score = 75
 console.log(score)
+*/
+
+//////////////////////////
+// Lesson 9 Strings
+
+// strings
+console.log('hello, world')
+
+let email = 'mario@thenetninja.co.uk'
+console.log(email)
+
+// string concatenation
+let firstName = 'Brandon'
+let lastName = 'Sanderson'
+
+let fullName = firstName + ' ' + lastName
+console.log(fullName)
+
+// getting characters
+console.log(fullName[2])
+
+// string length
+console.log(fullName.length)
+
+// string methods
+console.log(fullName.toUpperCase())
+let result = fullName.toLowerCase()
+console.log(result, fullName)
+
+let index = email.indexOf('@')
+console.log(index)
