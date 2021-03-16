5c7126476fae6bfdebede0315a8af696d7ddb341
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-03-16 10:16:34 +0100

    17. Type Convesion

--- chapter_1/sandbox.js
+++ chapter_1/sandbox.js
@@ -206,6 +206,7 @@ console.log(name > 'Crystal')
 //////////////////////////
 // Lesson 16 Loose vs Strict Comparison
 
+/*
 let age = 25
 
 // loose comparison (different types can still be equal)
@@ -221,3 +222,21 @@ console.log(age === 25)
 console.log(age === '25')
 console.log(age !== 25)
 console.log(age !== '25')
+*/
+
+//////////////////////////
+// Lesson 17 Type Conversion
+
+// type conversion
+let score = '100'
+
+// score = Number(score)
+// console.log(score + 1)
+// console.log(typeof score)
+
+// let result = Number('hello')
+// let result = String(50)
+// let result = Boolean(0)
+let result = Boolean('')
+
+console.log(result, typeof result)
