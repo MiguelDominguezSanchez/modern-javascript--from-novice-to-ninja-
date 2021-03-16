ea90e8dfe7765a44510a99c5d508063996f2396e
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-03-16 11:26:42 +0100

    24. Logical Operators

--- chapter_1/sandbox.js
+++ chapter_1/sandbox.js
@@ -322,6 +322,7 @@ if (password.length >= 8) {
 //////////////////////////
 // Lesson 23. Else & Else If
 
+/*
 // else if statements
 
 const password = 'p@ss'
@@ -332,4 +333,23 @@ if (password.length >= 12) {
 	console.log('that password is long enough!')
 } else {
 	console.log('password is not long enough')
+}
+*/
+
+//////////////////////////
+// Lesson 24. Logical Operators
+
+// logical operators - OR || and AND &&
+
+const password = 'p@ss'
+
+if (password.length >= 12 && password.includes('@')) {
+	console.log('that password is might strong')
+} else if (
+	password.length >= 8 ||
+	(password.includes('@') && password.length > 5)
+) {
+	console.log('that password is strong enough!')
+} else {
+	console.log('password is not strong enough')
 }
