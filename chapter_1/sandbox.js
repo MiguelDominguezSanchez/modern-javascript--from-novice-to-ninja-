b4c3c147a1ae004696ac4aa330a4156c710cd335
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-03-18 09:42:59 +0100

    40. 'this' Keyword

--- chapter_1/sandbox.js
+++ chapter_1/sandbox.js
@@ -664,6 +664,7 @@ console.log(typeof user)
 //////////////////////////
 // Lesson 39. Adding Methods
 
+/*
 let user = {
 	name: 'crystal',
 	age: 30,
@@ -684,3 +685,31 @@ user.logout()
 
 const name = 'mario'
 name.toUpperCase()
+*/
+
+//////////////////////////
+// Lesson 40. 'this' Keyword
+
+let user = {
+	name: 'crystal',
+	age: 30,
+	email: 'crystal@thenetninja.co.uk',
+	location: 'berlin',
+	blogs: ['why mac & cheese rules', '10 things to make with  marmite'],
+	login() {
+		console.log('the user logged in')
+	},
+	logout() {
+		console.log('the user logged out')
+	},
+	logBlogs() {
+		// console.log(this.blogs)
+		console.log('this user has written the following blogs:')
+		this.blogs.forEach(blog => {
+			console.log(blog)
+		})
+	},
+}
+
+user.logBlogs()
+console.log(this)
