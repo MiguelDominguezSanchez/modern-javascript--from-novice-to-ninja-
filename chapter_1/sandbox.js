f4b253637ba0a09ac3b4cbccfa7b052f6e26388a
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-03-18 15:04:56 +0100

    47. Other Ways to Query the DOM

--- chapter_1/sandbox.js
+++ chapter_1/sandbox.js
@@ -1,3 +1,7 @@
+/////////////////////////
+// 46. The Query Selector
+
+/*
 // const para = document.querySelector('body > h1:nth-child(1)')
 
 // console.log(para)
@@ -6,3 +10,21 @@ const paras = document.querySelectorAll('p')
 const errors = document.querySelectorAll('.error')
 
 console.log(errors)
+*/
+
+/////////////////////////
+// 47. Other Ways to Query the DOM
+
+// get an element by ID
+// const title = document.getElementById('page-title')
+// console.log(title)
+
+// get elements by their class name
+// const errors = document.getElementsByClassName('error')
+// console.log(errors)
+// console.log(errors[0])
+
+// get elements by their tag name
+const paras = document.getElementsByTagName('p')
+console.log(paras)
+console.log(paras[1])
