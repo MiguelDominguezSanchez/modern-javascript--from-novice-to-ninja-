5e546caeb76ce2316a2c68bb479ca4cba47e5301
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-03-19 12:04:52 +0100

    62. Basic Form Validation

--- chapter_7/sandbox.js
+++ chapter_7/sandbox.js
@@ -1,12 +1,27 @@
 const form = document.querySelector('.signup-form')
 // const username = document.querySelector('#username')
+const feedback = document.querySelector('.feedback')
 
 form.addEventListener('submit', e => {
 	e.preventDefault()
 	// console.log(username.value)
-	console.log(form.username.value)
+	// console.log(form.username.value)
+
+	// validation
+	const username = form.username.value
+	const usernamePattern = /^[a-zA-Z]{6,12}$/
+
+	if (usernamePattern.test(username)) {
+		// feedback good info
+		feedback.textContent = 'that username is valid!'
+	} else {
+		// feedback help info
+		feedback.textContent =
+			'username must contain letters only & be between 6 & 12 characters long'
+	}
 })
 
+/*
 // testing RegEx
 const username = '3434shaunp3656'
 const pattern = /^[a-z]{6,}$/
@@ -21,3 +36,4 @@ const pattern = /^[a-z]{6,}$/
 
 let result = username.search(pattern)
 console.log(result)
+*/
