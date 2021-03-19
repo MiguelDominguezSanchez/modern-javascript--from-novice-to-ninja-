f21e376e18e047181731ccb7f8877c72fd791223
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-03-19 12:25:18 +0100

    63. Keyboard Events

--- chapter_7/sandbox.js
+++ chapter_7/sandbox.js
@@ -1,6 +1,7 @@
 const form = document.querySelector('.signup-form')
 // const username = document.querySelector('#username')
 const feedback = document.querySelector('.feedback')
+const usernamePattern = /^[a-zA-Z]{6,12}$/
 
 form.addEventListener('submit', e => {
 	e.preventDefault()
@@ -9,7 +10,6 @@ form.addEventListener('submit', e => {
 
 	// validation
 	const username = form.username.value
-	const usernamePattern = /^[a-zA-Z]{6,12}$/
 
 	if (usernamePattern.test(username)) {
 		// feedback good info
@@ -37,3 +37,14 @@ const pattern = /^[a-z]{6,}$/
 let result = username.search(pattern)
 console.log(result)
 */
+
+// live feedback
+form.username.addEventListener('keyup', e => {
+	console.log(e)
+	// console.log(e.target.value, form.username.value)
+	if (usernamePattern.test(e.target.value)) {
+		form.username.setAttribute('class', 'success')
+	} else {
+		form.username.setAttribute('class', 'error')
+	}
+})
