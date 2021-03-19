bcf2113cb08493890866b3c1b4805f49fd4d72ec
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-03-19 17:54:53 +0100

    72. Map Method

--- chapter_9/sandbox.js
+++ chapter_9/sandbox.js
@@ -1,3 +1,9 @@
+/////////////////////////
+// Filter Method
+
+// filter method
+
+/*
 const scores = [10, 30, 15, 25, 50, 40, 5]
 
 // const filteredScores = scores.filter(score => {
@@ -21,3 +27,36 @@ const users = [
 const premiumUsers = users.filter(user => user.premium)
 
 console.log(premiumUsers)
+*/
+
+/////////////////////////
+// Map Method
+
+// map method
+
+const prices = [20, 10, 30, 25, 15, 40, 80, 5]
+
+// const salePrices = prices.map(price => {
+// 	return price / 2
+// })
+
+// const salePrices = prices.map(price => price / 2)
+// console.log(salePrices)
+
+const products = [
+	{ name: 'gold star', price: 20 },
+	{ name: 'mushroom', price: 40 },
+	{ name: 'green shells', price: 30 },
+	{ name: 'banana skin', price: 10 },
+	{ name: 'red shells', price: 50 },
+]
+
+const saleProducts = products.map(product => {
+	if (product.price > 30) {
+		return { name: product.name, price: product.price / 2 }
+	} else {
+		return product
+	}
+})
+
+console.log(saleProducts, products)
