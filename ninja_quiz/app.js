56ed3832d513035aa46a4ffbb8d814f777c302bf
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-03-19 17:11:33 +0100

    70. Interval & Animating the Score

--- ninja_quiz/app.js
+++ ninja_quiz/app.js
@@ -22,8 +22,17 @@ form.addEventListener('submit', e => {
 
 	// show result on page
 	scrollTo(0, 0)
-	result.querySelector('span').textContent = `${score}%`
 	result.classList.remove('d-none')
+
+	let output = 0
+	const timer = setInterval(() => {
+		result.querySelector('span').textContent = `${output}%`
+		if (output === score) {
+			clearInterval(timer)
+		} else {
+			output++
+		}
+	}, 10)
 })
 
 // window object (global object)
@@ -37,3 +46,14 @@ form.addEventListener('submit', e => {
 // setTimeout(() => {
 // 	alert('hello, ninjas')
 // }, 3000)
+
+// interval & animating the score
+
+// let i = 0
+// const timer = setInterval(() => {
+// 	console.log('hello')
+// 	i++
+// 	if (i === 5) {
+// 		clearInterval(timer)
+// 	}
+// }, 1000)
