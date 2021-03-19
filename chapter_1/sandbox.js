070a8a45bfa952017c39e6c4303fd79a9f586f10
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-03-19 10:03:35 +0100

    56. More DOM Events

--- chapter_1/sandbox.js
+++ chapter_1/sandbox.js
@@ -198,6 +198,7 @@ items.forEach(item => {
 /////////////////////////
 // 55. Event Bubbling (and delegation)
 
+/*
 const ul = document.querySelector('ul')
 // ul.remove()
 
@@ -225,4 +226,26 @@ ul.addEventListener('click', e => {
 	if (e.target.tagName === 'LI') {
 		e.target.remove()
 	}
+})
+*/
+
+/////////////////////////
+// 56. More DOM Events
+
+const copy = document.querySelector('.copy-me')
+
+copy.addEventListener('copy', () => {
+	console.log('OI! my content is copyright')
+})
+
+const box = document.querySelector('.box')
+
+box.addEventListener('mousemove', e => {
+	// console.log(e)
+	// console.log(e.offsetX, e.offsetY)
+	box.textContent = `x pos - ${e.offsetX} y pos - ${e.offsetY}`
+})
+
+document.addEventListener('wheel', e => {
+	console.log(e.pageX, e.pageY)
 })
