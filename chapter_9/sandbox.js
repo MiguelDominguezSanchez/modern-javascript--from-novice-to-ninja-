2d79b7ac39f8b02b238956c35bcaaeca568a4b93
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-03-19 18:19:25 +0100

    73. Reduce Method

--- chapter_9/sandbox.js
+++ chapter_9/sandbox.js
@@ -32,6 +32,7 @@ console.log(premiumUsers)
 /////////////////////////
 // Map Method
 
+/*
 // map method
 
 const prices = [20, 10, 30, 25, 15, 40, 80, 5]
@@ -60,3 +61,47 @@ const saleProducts = products.map(product => {
 })
 
 console.log(saleProducts, products)
+*/
+
+/////////////////////////
+// Reduce Method
+
+// reduce method
+// const scores = [10, 20, 60, 40, 70, 90, 30]
+
+// const result = scores.reduce((acc, curr) => {
+// 	if (curr > 50) {
+// 		acc++
+// 	}
+// 	return acc
+// }, 0)
+
+// console.log(result)
+
+const scores = [
+	{ player: 'mario', score: 50 },
+	{ player: 'yoshi', score: 30 },
+	{ player: 'mario', score: 70 },
+	{ player: 'crystal', score: 60 },
+	{ player: 'mario', score: 50 },
+	{ player: 'yoshi', score: 30 },
+	{ player: 'mario', score: 70 },
+	{ player: 'crystal', score: 60 },
+	{ player: 'mario', score: 90 },
+	{ player: 'yoshi', score: 30 },
+	{ player: 'mario', score: 30 },
+	{ player: 'crystal', score: 60 },
+	{ player: 'mario', score: 50 },
+	{ player: 'yoshi', score: 30 },
+	{ player: 'mario', score: 80 },
+	{ player: 'crystal', score: 60 },
+]
+
+const marioTotal = scores.reduce((acc, curr) => {
+	if (curr.player === 'mario') {
+		acc + curr.score
+	}
+	return acc
+}, 0)
+
+console.log(marioTotal)
