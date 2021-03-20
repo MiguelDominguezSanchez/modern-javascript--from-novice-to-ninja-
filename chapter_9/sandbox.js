8704f5c3741b156d9c12b9a03890a6321f8e64ed
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-03-20 08:49:11 +0100

    76. Chaining Array Methods

--- chapter_9/sandbox.js
+++ chapter_9/sandbox.js
@@ -127,6 +127,7 @@ console.log(firstHighScore)
 /////////////////////////
 // Sort Method
 
+/*
 // example 1 - sorting strings
 const names = ['mario', 'shaun', 'chun-li', 'yoshi', 'luigi']
 
@@ -170,3 +171,29 @@ const players = [
 players.sort((a, b) => b.score - a.score)
 
 console.log(players)
+*/
+
+/////////////////////////
+// 76. Chaining Array Methods
+
+// chaining array methods
+
+const products = [
+	{ name: 'gold star', price: 30 },
+	{ name: 'green shell', price: 10 },
+	{ name: 'red shell', price: 40 },
+	{ name: 'banana skin', price: 5 },
+	{ name: 'mushroom', price: 50 },
+]
+
+// const filtered = products.filter(product => product.price > 20)
+
+// const promos = filtered.map(product => {
+// 	return `the ${product.name} is ${product.price / 2} pounds`
+// })
+
+const promos = products
+	.filter(product => product.price > 20)
+	.map(product => `the ${product.name} is ${product.price / 2} pounds`)
+
+console.log(promos)
