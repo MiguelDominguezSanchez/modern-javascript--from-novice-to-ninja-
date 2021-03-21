7e4362bb0ed29f6437b05de6b120523230acf23b
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-03-21 17:09:07 +0100

    95. Chaining Promises

--- chapter_12/sandbox.js
+++ chapter_12/sandbox.js
@@ -117,6 +117,7 @@ getTodos('todos/luigi.json', (err, data) => {
 ///////////////////////
 // 92. JSON Data
 
+/*
 const getTodos = resource => {
 	return new Promise((resolve, reject) => {
 		const request = new XMLHttpRequest()
@@ -169,3 +170,41 @@ getTodos('todos/luigis.json')
 // 	.catch(err => {
 // 		console.log(err)
 // 	})
+*/
+
+///////////////////////
+// 95. Chaining Promises
+
+const getTodos = resource => {
+	return new Promise((resolve, reject) => {
+		const request = new XMLHttpRequest()
+
+		request.addEventListener('readystatechange', () => {
+			if (request.readyState === 4 && request.status === 200) {
+				const data = JSON.parse(request.responseText)
+				resolve(data)
+			} else if (request.readyState === 4) {
+				reject('error getting resource')
+			}
+		})
+
+		request.open('GET', resource)
+		request.send()
+	})
+}
+
+getTodos('todos/luigi.json')
+	.then(data => {
+		console.log('promise 1 resolved:', data)
+		return getTodos('todos/mario.json')
+	})
+	.then(data => {
+		console.log('promise 2 resolved:', data)
+		return getTodos('todos/shauns.json')
+	})
+	.then(data => {
+		console.log('promise 3 resolved:', data)
+	})
+	.catch(err => {
+		console.log('promise rejected:', err)
+	})
