7fdfc302b4b064684d263e9c3fbe4d58c3643800
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-03-21 10:04:39 +0100

    90. Response Status

--- chapter_12/sandbox.js
+++ chapter_12/sandbox.js
@@ -16,6 +16,7 @@ console.log(4)
 ///////////////////////
 // 89. Making HTTP Request (XMR)
 
+/*
 const request = new XMLHttpRequest()
 
 request.addEventListener('readystatechange', () => {
@@ -26,4 +27,22 @@ request.addEventListener('readystatechange', () => {
 })
 
 request.open('GET', 'https://jsonplaceholder.typicode.com/todos/')
+request.send()
+*/
+
+///////////////////////
+// 90. Response Status
+
+const request = new XMLHttpRequest()
+
+request.addEventListener('readystatechange', () => {
+	// console.log(request, request.readyState)
+	if (request.readyState === 4 && request.status === 200) {
+		console.log(request, request.responseText)
+	} else if (request.readyState === 4) {
+		console.log('could not fetch the data')
+	}
+})
+
+request.open('GET', 'https://jsonplaceholder.typicode.com/todoss/')
 request.send()
