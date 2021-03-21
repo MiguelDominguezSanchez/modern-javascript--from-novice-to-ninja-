02f165b83d533fb66bcde8ea1fbb18dffd44076b
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-03-21 13:00:50 +0100

    91. Callback Functions

--- chapter_12/sandbox.js
+++ chapter_12/sandbox.js
@@ -33,6 +33,7 @@ request.send()
 ///////////////////////
 // 90. Response Status
 
+/*
 const request = new XMLHttpRequest()
 
 request.addEventListener('readystatechange', () => {
@@ -44,5 +45,38 @@ request.addEventListener('readystatechange', () => {
 	}
 })
 
-request.open('GET', 'https://jsonplaceholder.typicode.com/todoss/')
+request.open('GET', 'https://jsonplaceholder.typicode.com/todos/')
 request.send()
+*/
+
+///////////////////////
+// 91. Callback Functions
+const getTodos = callback => {
+	const request = new XMLHttpRequest()
+
+	request.addEventListener('readystatechange', () => {
+		if (request.readyState === 4 && request.status === 200) {
+			callback(undefined, request.responseText)
+		} else if (request.readyState === 4) {
+			callback('could not fetch data', undefined)
+		}
+	})
+
+	request.open('GET', 'https://jsonplaceholder.typicode.com/todos/')
+	request.send()
+}
+
+console.log(1)
+console.log(2)
+
+getTodos((err, data) => {
+	console.log('callback fired')
+	if (err) {
+		console.log(err)
+	} else {
+		console.log(data)
+	}
+})
+
+console.log(3)
+console.log(4)
