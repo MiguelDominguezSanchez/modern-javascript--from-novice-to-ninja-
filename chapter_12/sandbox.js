c2063607d7c120ab9fb34af586d062f9dc8f8966
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-03-21 13:12:43 +0100

    92. JSON Data

--- chapter_12/sandbox.js
+++ chapter_12/sandbox.js
@@ -51,6 +51,8 @@ request.send()
 
 ///////////////////////
 // 91. Callback Functions
+
+/*
 const getTodos = callback => {
 	const request = new XMLHttpRequest()
 
@@ -80,3 +82,32 @@ getTodos((err, data) => {
 
 console.log(3)
 console.log(4)
+*/
+
+///////////////////////
+// 92. JSON Data
+
+const getTodos = callback => {
+	const request = new XMLHttpRequest()
+
+	request.addEventListener('readystatechange', () => {
+		if (request.readyState === 4 && request.status === 200) {
+			const data = JSON.parse(request.responseText)
+			callback(undefined, data)
+		} else if (request.readyState === 4) {
+			callback('could not fetch data', undefined)
+		}
+	})
+
+	request.open('GET', 'todos.json')
+	request.send()
+}
+
+getTodos((err, data) => {
+	console.log('callback fired')
+	if (err) {
+		console.log(err)
+	} else {
+		console.log(data)
+	}
+})
